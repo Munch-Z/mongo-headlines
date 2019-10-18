d131ea6eee4a61a2786268a0e56388a8e65204ba
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-10-17 19:57:16 -0700

    Ayye

--- server.js
+++ server.js
@@ -17,8 +17,11 @@ app.use(express.static("client/build"));
 
 //Routes
 app.use("/api", apiRoutes);
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+app.get("*", (req, res) => {
+  res.redirect("/");
 });
 
 app.listen(PORT, err => {
